59ff1a45e631a243a1652ced14e176559913122d
Henrique <henrique.rodrigues7@hotmail.com>
2023-06-25 19:20:24 -0300

    feat(prisma): install prisma and schema

---
+++ backend/src/infra/prisma/migrations/20230625215603_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Contact" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "phone" TEXT NOT NULL
+);
